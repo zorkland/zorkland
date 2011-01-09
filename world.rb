1d7026175c69dd409c6ec9c495a8ca80c0dda161
WTN <wtn@notational.net>
2011-01-09 11:22:04 -0800

    louie's stopping point for day 1

--- world.rb
+++ world.rb
@@ -1,41 +1,63 @@
+require 'set'
+
+class Location
+  attr_reader :name, :description, :exits
+
+  def initialize(name, description, exits)
+    @name = name
+    @description = description
+    @exits = exits
+  end
+end
+
 class World
+  attr_reader :current_location
  
-  def initialize(current_location)
-    @current_location = current_location
+  def initialize
+    @current_location = :forest
+    @places = {
+      :forest =>  Location.new('forest','a dark scary forest', Set[:tree, :castle]),
+      :castle => Location.new('castle','fat castle', Set[:forest, :pit]),
+      :pit => Location.new('pit','a dark tar filled pit', Set[:castle]),
+      :portal => Location.new('portal','a mysterious woosh woosh portal', Set[:forest, :castle, :pit, :tree]),
+      :tree => Location.new('tree','a mystic tree', Set[:portal, :forest]),
+    }
   end
 
   def where_am_i
-    @current_location
+    @places[@current_location]
   end
   
   def current_description
-    @current_location.description
+    where_am_i.description
   end
   
   def go_to(new_location)
+    if ! current_exits.include?(new_location)
+      raise "cannot skip locations"
+    end
     @current_location = new_location
   end
-    
-end
 
-class Location
-  attr_reader :name, :description
-  
-  def initialize(name, description)
-    @name = name
-    @description = description
+  def current_exits
+    where_am_i.exits
   end
+
 end
 
+w=World.new
+puts "hello traveler, welcome to the land of EDDDYYYYYY"
+while(true)
+ begin
+  puts "you are currently at " + w.where_am_i.name + ", " + w.current_description
+  puts "you can travel to: " + w.current_exits.to_a.join(', ')
+  puts "where would you like to go?"
+  STDOUT.flush  
+  desired_location = gets.chomp.to_sym  
+  w.go_to(desired_location)
+rescue
+  puts 'STOP TRYING TO CHEAT R U N MURCAN?!?!'
+  retry
+end
 
-
-# a = World.new
-# a.description
-# a.description = "the forest of will, green and scary (with monsters)"
-# 
-# Animals
-#   Cats
-#   Dogs
-#   Snakes
-# Names
-# Colors
+end
--- world_spec.rb
+++ world_spec.rb
@@ -1,34 +1,48 @@
-require './world.rb'
+require './v2.rb'
+require 'set'
 
 describe World do
   before do
-    @forest = Location.new('forest','a dark scary forest')
-    @castle = Location.new('castle','fat castle')
-    @world = World.new(@forest)
+    @world = World.new
+    @forest = Location.new('forest','a dark scary forest', Set[:tree, :castle])
   end
 
   it 'starts out in the forest' do
-    @world.where_am_i.should eq @forest
+    @world.current_location.should eq :forest
   end
 
-  it '#description returns description of current loc' do
-    @world.current_description.should eq @forest.description
+  it '#current_description returns description of current loc' do
+    @world.current_description.should eq 'a dark scary forest'
   end
 
-  it 'lets you go to the castle and return' do
-    @world.go_to(@castle)
-    @world.where_am_i.should eq @castle
-    @world.current_description.should eq @castle.description
-    @world.go_to(@forest)
-    @world.where_am_i.should eq @forest
-    @world.current_description.should eq @forest.description 
+  it 'shows you list of possible exits' do
+    @world.current_exits.should eq Set[:castle, :tree]
   end
+
+  it 'lets you go to possible places' do
+    @world.go_to(:castle)
+    @world.current_location.should eq :castle
+    @world.go_to(:forest)
+    @world.current_location.should eq :forest
+  end
+  
+  it 'blocks impossible places' do
+    proc { @world.go_to(:portal) }.should raise_error # eq "cannot skip locations"
+  end
+
 end
 
 describe Location do
+  before do
+    @forest = Location.new('forest','a dark scary forest', Set[:tree, :castle])
+  end
+  
   it 'has a description and name' do
-    location = Location.new('name','description')
-    location.description.should eq 'description'
-    location.name.should eq 'name'
+    @forest.description.should eq 'a dark scary forest'
+    @forest.name.should eq 'forest'
+  end
+  
+  it 'has exits' do
+    @forest.exits.should eq Set[:tree, :castle]
   end
 end
